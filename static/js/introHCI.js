6cb53acba2686e6c6521cc83f2cc8c01234ef163
Jingya Huang <jih201@ucsd.edu>
2017-01-19 10:33:59 -0800

    add the javascript

---
+++ static/js/introHCI.js
@@ -0,0 +1,27 @@
+'use strict';
+
+// Call this function when the page loads (the "ready" event)
+$(document).ready(function() {
+    initializePage();
+})
+/*
+ * Function that is called when the document is read.
+ */
+function initializePage() {
+        $(“#testjs”).click(function(e)) {
+            $(‘.jumbotron h1’).text(“Javascript is connected”);
+        });
+
+        // Add any additional listeners here 
+        // example: $(“#div-id”).click(functionToCall);
+        $(“a.thumbnail”).click(projectClick);
+}
+
+function projectClick(e) {
+    // Cancel the default action, which prevents the page from reloading 
+    e.preventDefault();
+    // In an event listener, $(this) is the element that fired the event 
+    var projectTitle = $(this).find("p").text();
+    var jumbotronHeader = $("#jumbotron h1");
+    jumbotronHeader.text(projectTitle);
+}
